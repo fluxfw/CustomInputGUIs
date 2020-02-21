db416a118f7b96f3de75d311946d578476ac4e27
Florian Wyss <fw@studer-raimann.ch>
2020-02-21 07:40:55 +0100

    Fix

--- src/MultiSelectSearchNewInputGUI/class.ObjectsAjaxAutoCompleteCtrl.php
+++ src/MultiSelectSearchNewInputGUI/class.ObjectsAjaxAutoCompleteCtrl.php
@@ -28,9 +28,9 @@ class ObjectsAjaxAutoCompleteCtrl extends AbstractAjaxAutoCompleteCtrl
      * ObjectsAjaxAutoCompleteCtrl constructor
      *
      * @param string $type
-     * @param bool $ref_id
+     * @param bool   $ref_id
      */
-    public function __construct(string $type,bool $ref_id=false)
+    public function __construct(string $type, bool $ref_id = false)
     {
         parent::__construct();
 
@@ -45,7 +45,7 @@ class ObjectsAjaxAutoCompleteCtrl extends AbstractAjaxAutoCompleteCtrl
     public function searchOptions(string $search = null) : array
     {
         $result = self::dic()->database()->queryF('
-SELECT '.($this->ref_id?'object_reference.ref_id':'object_data.obj_id').', title
+SELECT ' . ($this->ref_id ? 'object_reference.ref_id' : 'object_data.obj_id') . ', title
 FROM object_data
 INNER JOIN object_reference ON object_data.obj_id=object_reference.obj_id
 WHERE type=%s
@@ -64,14 +64,14 @@ AND object_reference.deleted IS NULL
     public function fillOptions(array $ids) : array
     {
         $result = self::dic()->database()->queryF('
-SELECT '.($this->ref_id?'object_reference.ref_id':'object_data.obj_id').', title
+SELECT ' . ($this->ref_id ? 'object_reference.ref_id' : 'object_data.obj_id') . ', title
 FROM object_data
 INNER JOIN object_reference ON object_data.obj_id=object_reference.obj_id
 WHERE type=%s
 AND object_reference.deleted IS NULL
 AND ' . self::dic()
                 ->database()
-                ->in(($this->ref_id?'object_reference.ref_id':'object_data.obj_id'), $ids, false, ilDBConstants::T_INTEGER) . ' ORDER BY title ASC', [ilDBConstants::T_TEXT], [$this->type]);
+                ->in(($this->ref_id ? 'object_reference.ref_id' : 'object_data.obj_id'), $ids, false, ilDBConstants::T_INTEGER) . ' ORDER BY title ASC', [ilDBConstants::T_TEXT], [$this->type]);
 
         return $this->formatObjects(self::dic()->database()->fetchAll($result));
     }
@@ -87,7 +87,7 @@ AND ' . self::dic()
         $formatted_objects = [];
 
         foreach ($objects as $object) {
-            $formatted_objects[$object[($this->ref_id?'ref_id':'obj_id')]] = $object["title"];
+            $formatted_objects[$object[($this->ref_id ? 'ref_id' : 'obj_id')]] = $object["title"];
         }
 
         return $formatted_objects;
