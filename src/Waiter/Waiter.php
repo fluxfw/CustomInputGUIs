899a2c05d9d2fc0f12dbac99db79f68f6fe67f72
Theodor Truffer <tt@studer-raimann.ch>
2021-02-18 09:33:08 +0100

    waiter: show bytes

--- src/Waiter/Waiter.php
+++ src/Waiter/Waiter.php
@@ -2,13 +2,12 @@
 
 namespace srag\CustomInputGUIs\Waiter;
 
+use ilTemplate;
 use srag\DIC\DICTrait;
 
 /**
  * Class Waiter
- *
  * @package srag\CustomInputGUIs\Waiter
- *
  * @author  studer + raimann ag - Team Custom 1 <support-custom1@studer-raimann.ch>
  */
 final class Waiter
@@ -33,28 +32,26 @@ final class Waiter
     /**
      * Waiter constructor
      */
-    private function __construct()
-    {
-
-    }
-
+    private function __construct(){}
 
     /**
-     * @param string $type
+     * @param string     $type
+     * @param ilTemplate $ilTemplate
      */
-    public static final function init(string $type)/*: void*/
+    public static final function init(string $type, $ilTemplate = null)/*: void*/
     {
+        $ilTemplate = $ilTemplate ?? self::dic()->ui()->mainTemplate();
         if (self::$init === false) {
             self::$init = true;
 
             $dir = __DIR__;
             $dir = "./" . substr($dir, strpos($dir, "/Customizing/") + 1);
 
-            self::dic()->ui()->mainTemplate()->addCss($dir . "/css/waiter.css");
+            $ilTemplate->addCss($dir . "/css/waiter.css");
 
-            self::dic()->ui()->mainTemplate()->addJavaScript($dir . "/js/waiter.min.js");
+            $ilTemplate->addJavaScript($dir . "/js/waiter.min.js");
         }
 
-        self::dic()->ui()->mainTemplate()->addOnLoadCode('il.waiter.init("' . $type . '");');
+        $ilTemplate->addOnLoadCode('il.waiter.init("' . $type . '");');
     }
 }
